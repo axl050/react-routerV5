b7c7a679521a522e6d881ea3a72138d53312c230
axl050 <axelgit@homtail.com>
2022-06-27 02:24:29 -0300

    router-version5

---
+++ src/components/ConceptosBasicos.js
@@ -0,0 +1,21 @@
+import { Link, Route, Routes } from "react-router-dom";
+
+const ConceptosBasicos = () => {
+  const Contacto = () => {
+    return <h3>Contacto</h3>;
+  };
+
+  return (
+    <div>
+      <h2>Conceptos Basicos</h2>
+
+      <Routes>
+        <Route path="/" element={<h2>Home</h2>}></Route>
+        <Route path="/acerca" element={<h3>Acerca</h3>} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default ConceptosBasicos;
